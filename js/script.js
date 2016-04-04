511dd7437762b1d640a2365578079003c43ea7c5
Kseniia Anishchenko <kseninavoi@gmail.com>
2016-04-03 20:56:06 -0700

    Работа над замечаниями

--- js/script.js
+++ js/script.js
@@ -1,60 +1,51 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var toggleWrapper = document.querySelector('.main-nav__toggle-wrapper');
-    var nav = document.querySelector('.main-nav');
-    var topMenu = document.querySelector('.main-header__menu');
+    var staticHeader = document.querySelector(".main-header--static");
+    if (staticHeader){
+        staticHeader.classList.remove("main-header--static");
+    }
+    
+    var toggleWrapper = document.querySelector(".main-nav__toggle-wrapper");
 
-    toggleWrapper.addEventListener('click', function(event) {
+    toggleWrapper.addEventListener("click", function(event) {
         event.preventDefault();
         toggleMenu();
     });
     
     toggleMenu();
     function toggleMenu(){
-        var mainNav = document.querySelector(".main-nav");
-        var visibleButton = document.querySelector(".main-nav-toggle:not(.hidden)");
-        var hiddenButton = document.querySelector(".main-nav-toggle.hidden");
+        var statesList = {
+            OPENED: "main-header__wrapper--opened",
+            CLOSED: "main-header__wrapper--closed"
+        };
         
-        visibleButton.classList.add("hidden");
-        hiddenButton.classList.remove("hidden");
-        
-        if (mainNav.classList.contains("hidden")){
-            mainNav.classList.remove("hidden");
-        }else{
-            mainNav.classList.add("hidden");
-        }
-        
-        var visibleHeader = document.querySelector(".main-header__menu:not(.main-header__menu--opened");
-        var hiddenHeader = document.querySelector(".main-header__menu.main-header__menu--opened");
-        
-        if (hiddenHeader){
-            hiddenHeader.classList.remove("main-header__menu--opened");
+        var headerWrapper = document.querySelector(".main-header__wrapper")
+       
+        var toAdd, toDelete;
+        if (headerWrapper.classList.contains(statesList.OPENED)){
+           toAdd = statesList.CLOSED;
+           toDelete = statesList.OPENED;
         }
         else{
-            visibleHeader.classList.add("main-header__menu--opened");
+           toAdd = statesList.OPENED;
+           toDelete = statesList.CLOSED;
         }
+       
+        headerWrapper.classList.add(toAdd);
+        headerWrapper.classList.remove(toDelete);
     }
-});
-
-document.addEventListener("DOMContentLoaded", function(){
+    
     var centerLatLng = new google.maps.LatLng(59.936204,30.322786);
-
-var mapOptions = {
-
-zoom: 18,
-
-center: centerLatLng,
-
-scrollwheel: false,
-
-mapTypeId: google.maps.MapTypeId.ROADMAP
-
-};
-
-                var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-                var image = 'img/icon-map-marker.svg';
-                var marker = new google.maps.Marker({
-                    position: centerLatLng,
-                    map: map,
-                    icon: image
-                });
+    var mapOptions = {
+        zoom: 18,
+        center: centerLatLng,
+        scrollwheel: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var image = "img/icon-map-marker.svg";
+    var marker = new google.maps.Marker({
+        position: centerLatLng,
+        map: map,
+        icon: image
+    });
 });
